6b91cb800454e6553c193c1af6dba13b024aa5ee
nicosampler <nf.dominguez.87@gmail.com>
2020-03-02 15:56:53 -0300

    remove unused code

--- src/components/inputs/Select/index.tsx
+++ src/components/inputs/Select/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
