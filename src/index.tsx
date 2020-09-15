bcff040cbe976db934f83d42297df2f260381efd
nicosampler <nf.dominguez.87@gmail.com>
2020-09-15 19:45:32 -0300

    Add safe-connect app

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Compound from "./apps/Compound";
-import Testing from "./apps/Testing";
+import SafeConnect from "./apps/SafeConnect";
 import TxBuilder from "./apps/TxBuilder";
 import GlobalStyles from "./global";
 
@@ -20,8 +20,8 @@ ReactDOM.render(
         <Route path="/tx-builder">
           <TxBuilder />
         </Route>
-        <Route path="/testing">
-          <Testing />
+        <Route path="/safe-connect">
+          <SafeConnect />
         </Route>
         <Route
           path="/"
@@ -35,7 +35,7 @@ ReactDOM.render(
                   <Link to="/tx-builder">Tx Builder</Link>
                 </div>
                 <div>
-                  <Link to="/testing">Testing</Link>
+                  <Link to="/safe-connect">Safe Connect</Link>
                 </div>
               </>
             );
---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+export const chainIdByNetwork = {
+  mainnet: 1,
+  morden: 2,
+  ropsten: 3,
+  rinkeby: 4,
+  goerli: 5,
+  kovan: 42,
+  unknown: 1337,
+};
+
+export const gnosisUrlByNetwork = {
+  mainnet: "https://gnosis-safe.io/app",
+  morden: null,
+  ropsten: null,
+  rinkeby: "https://safe-team-rinkeby.staging.gnosisdev.com",
+  goerli: null,
+  kovan: null,
+  unknown: null,
+};
