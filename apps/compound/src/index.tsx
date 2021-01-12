a924f443fc62d9c566a5d2d00807277513cedc40
Mikhail Mikheev <mmvsha73@gmail.com>
2021-01-12 13:38:27 +0400

    linting changes

--- apps/compound/src/index.tsx
+++ apps/compound/src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { ThemeProvider } from "styled-components"
-import { SafeProvider } from "@gnosis.pm/safe-apps-react-sdk"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { SafeProvider } from '@gnosis.pm/safe-apps-react-sdk';
 
-import GlobalStyles from "./global"
-import * as serviceWorker from "./serviceWorker"
-import App from "./App"
-import getTheme from "./customTheme"
+import GlobalStyles from './global';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import getTheme from './customTheme';
 
-const theme = getTheme()
+const theme = getTheme();
 
 ReactDOM.render(
   <>
@@ -19,10 +19,10 @@ ReactDOM.render(
       </SafeProvider>
     </ThemeProvider>
   </>,
-  document.getElementById("root")
-)
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
