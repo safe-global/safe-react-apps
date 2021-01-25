8cee21e25281ba4be9e53554af0722cf4807f5a3
Mikhail <mmvsha73@gmail.com>
2021-01-25 20:27:53 +0400

    env var tests

--- scripts/deploy_pr.sh
+++ scripts/deploy_pr.sh
@@ -12,22 +12,12 @@ function deploy_app_pr {
   REPO_NAME_ALPHANUMERIC=$(echo $REPO_NAME | sed 's/[^a-zA-Z0-9]//g')
 
   REVIEW_FEATURE_FOLDER="$REPO_NAME_ALPHANUMERIC/$PULL_REQUEST_NUMBER"
-  echo REPO_NAME_ALPHANUMERIC
-  echo PULL_REQUEST_NUMBER
+  echo $REPO_NAME_ALPHANUMERIC
+  echo $PULL_REQUEST_NUMBER
   # Deploy app project
   # aws s3 sync build s3://${REVIEW_BUCKET_NAME}/${REVIEW_FEATURE_FOLDER}/$1 --delete
 }
-
-function publish_pull_request_urls_in_github {
-  REVIEW_FEATURE_URL="https://$PULL_REQUEST_NAME--$REPO_NAME_ALPHANUMERIC.$REVIEW_ENVIRONMENT_DOMAIN/$REACT_APP_NETWORK/app"
-
-  # Using the Issues api instead of the PR api
-  # Done so because every PR is an issue, and the issues api allows to post general comments,
-  # while the PR api requires that comments are made to specific files and specific commits
-  GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
-  curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment:\r\n '${REVIEW_FEATURE_URL}' \r\n"}'
-}
-
+echo $AWS_ACCESS_KEY_ID
 # Only:
 # - Pull requests
 # - Security env variables are available. PR from forks don't have them.
@@ -39,5 +29,4 @@ then
       deploy_app_pr app
     fi; 
   done
-  # publish_pull_request_urls_in_github
 fi
