d28c5bd71aae25ee1b4c4d6030052c83732f7782
Mikhail <mmvsha73@gmail.com>
2021-01-26 17:03:31 +0400

    test app path

--- scripts/deploy_pr.sh
+++ scripts/deploy_pr.sh
@@ -9,14 +9,16 @@ function deploy_app_pr {
 
   REVIEW_FEATURE_FOLDER="$REPO_NAME_ALPHANUMERIC/$PULL_REQUEST_NUMBER"
 
+  APP_PATH="./apps/$1/build"
+  echo $APP_PATH
   # Deploy app project
-  aws s3 sync build s3://${REVIEW_BUCKET_NAME}/${REVIEW_FEATURE_FOLDER}/$1 --delete
+  # aws s3 sync $APP_PATH s3://${REVIEW_BUCKET_NAME}/${REVIEW_FEATURE_FOLDER}/$1 --delete
 }
 
 # Only:
 # - Pull requests
 # - Security env variables are available. PR from forks don't have them.
-if [ -n "$AWS_ACCESS_KEY_ID" ]; then
+if [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
   echo "Running the script"
   for file in ./apps/*/; do
     if [[ -d "$file" && ! -L "$file" ]]; then
