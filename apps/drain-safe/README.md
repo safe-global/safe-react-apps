88f1096bd3f497f9490066d05e7372584b33d27e
katspaugh <katspaugh@users.noreply.github.com>
2021-05-03 14:11:18 +0200

    Drain Account app (#120)
    * Drain Account app
    
    * Add myself to the CLA list
    
    * Update the readme
    
    * Add the app to the deployment list
    
    * PR comments; run prettier
    
    * Poll for the transaction info and display balances as 0
    
    * Display a message when the transaction is sent
    
    * Rm etherscan button
    
    * Add icon attribution; change the button to contained
    
    * Rm the redundant safe address
    
    * Use web3-eth-abi to avoid instantiating web3
    
    * Add an error message for invalid addresses
    
    * Add tests for utils
    
    * Rm lerna, use yarn workspaces
    
    * Rm unused deps; move dev deps; rm unfinished tests
    
    * Restore material-ui/core; rm unused setup code
    
    * Rm duplicate deps; don't assume networks; add .nvrmc
    
    * Replace client-gateway with transaction API
    
    * Use safe-react-components from NPM
    
    * Use safe-react-components 0.6.0
    
    * Fix the Ether icon path
    
    * Reorganize components and hooks
    
    * Rename gateway -> api
    
    * Add logo in the title

---
+++ apps/drain-safe/README.md
@@ -0,0 +1,19 @@
+# Drain Safe
+
+This project is based on the Safe App CRA template.
+
+<img width="857" alt="Screenshot 2021-04-15 at 15 55 18" src="https://user-images.githubusercontent.com/381895/114881122-002d6e80-9e03-11eb-831b-59cbda8b529d.png">
+
+## Run locally
+
+Install:
+
+```
+yarn
+```
+
+Run:
+
+```
+yarn start
+```
---
+++ apps/drain-safe/src/components/App.tsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import { Title, TextField, Text } from '@gnosis.pm/safe-react-components';
+import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
+import web3Utils from 'web3-utils';
+import { Asset } from '../utils/api';
+import useBalances from '../hooks/use-balances';
+import { tokenToTx } from '../utils/sdk-helpers';
+import FormContainer from './FormContainer';
+import Flex from './Flex';
+import Logo from './Logo';
+import Balances from './Balances';
+import SubmitButton from './SubmitButton';
+import CancelButton from './CancelButton';
+
+const App: React.FC = () => {
+  const { sdk, safe } = useSafeAppsSDK();
+  const { assets, error: balancesError }: { assets: Asset[]; error?: Error } = useBalances(
+    safe.safeAddress,
+    safe.network,
+  );
+  const [emptyAssets, setEmptyAssets] = useState<Asset[] | null>(null);
+  const [submitting, setSubmitting] = useState(false);
+  const [toAddress, setToAddress] = useState<string>('');
+  const [isFinished, setFinished] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+
+  const onError = (userMsg: string, err: Error) => {
+    setError(`${userMsg}: ${err.message}`);
+    console.error(userMsg, err);
+  };
+
+  const resetMessages = () => {
+    setError('');
+    setFinished(false);
+  };
+
+  const sendTxs = async (): Promise<string> => {
+    const txs = assets.map((item) => tokenToTx(toAddress, item));
+    const data = await sdk.txs.send({ txs });
+    return data.safeTxHash;
+  };
+
+  const submitTx = async (): Promise<void> => {
+    if (!web3Utils.isAddress(toAddress)) {
+      setError('Please enter a valid recipient address');
+      return;
+    }
+
+    resetMessages();
+    setSubmitting(true);
+
+    try {
+      await sendTxs();
+    } catch (e) {
+      setSubmitting(false);
+      onError('Failed sending transactions', e);
+      return;
+    }
+
+    setSubmitting(false);
+    setFinished(true);
+    setToAddress('');
+
+    setEmptyAssets(
+      assets.map((item) => ({
+        ...item,
+        balance: '0',
+        fiatBalance: '0',
+      })),
+    );
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    submitTx();
+  };
+
+  const onCancel = () => {
+    resetMessages();
+    setSubmitting(false);
+  };
+
+  const onToAddressChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setToAddress(e.target.value);
+    resetMessages();
+  };
+
+  useEffect(() => {
+    if (balancesError) {
+      onError('Failed fetching balances', balancesError);
+    }
+  }, [balancesError]);
+
+  return (
+    <FormContainer onSubmit={onSubmit} onReset={onCancel}>
+      <Flex>
+        <Logo />
+        <Title size="md">Drain Account</Title>
+      </Flex>
+
+      <Balances assets={emptyAssets || assets} />
+
+      {error && <Text size="lg">{error}</Text>}
+
+      {isFinished && (
+        <Text size="lg">
+          The transaction has been created.{' '}
+          <span role="img" aria-label="success">
+            ✅
+          </span>
+          <br />
+          Refresh the app when it’s executed.
+        </Text>
+      )}
+
+      {!submitting && <TextField onChange={onToAddressChange} value={toAddress} label="Recipient" />}
+
+      {submitting ? <CancelButton /> : <SubmitButton />}
+    </FormContainer>
+  );
+};
+
+export default App;
