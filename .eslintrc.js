d2b40c3fb030f0082f2d5dd8e71caa7aed5239c1
Mikhail Mikheev <mmvsha73@gmail.com>
2021-06-30 18:20:41 +0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
