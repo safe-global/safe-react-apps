c015250a671f595c3ac5f9e10deb55a8c2377c97
Yago Pérez Vázquez <yagoperez@outlook.com>
2021-11-02 17:22:51 +0100

    Sort list elements

--- apps/drain-safe/src/components/Balances.tsx
+++ apps/drain-safe/src/components/Balances.tsx
@@ -1,8 +1,9 @@
-import { Table, Checkbox } from '@gnosis.pm/safe-react-components';
+import { Table, Checkbox, TableSortDirection } from '@gnosis.pm/safe-react-components';
 import { formatTokenValue, formatCurrencyValue } from '../utils/formatters';
 import Icon from './Icon';
 import { TokenBalance, TokenInfo } from '@gnosis.pm/safe-apps-sdk';
 import Flex from './Flex';
+import { useState } from 'react';
 
 const CURRENCY = 'USD';
 
@@ -15,6 +16,39 @@ const ethToken: TokenInfo = {
   address: '',
 };
 
+function resolvePath(path: string, obj: any) {
+  const props = path.split('.');
+  return props.reduce((prev, curr) => prev && prev[curr], obj);
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: string) {
+  let item1 = resolvePath(orderBy, a);
+  let item2 = resolvePath(orderBy, b);
+
+  if (item2 < item1) {
+    return -1;
+  }
+
+  if (item2 > item1) {
+    return 1;
+  }
+
+  return 0;
+}
+
+function getComparator(
+  order: TableSortDirection,
+  orderBy: string | undefined,
+): (a: TokenBalance, b: TokenBalance) => number {
+  if (!orderBy) {
+    return () => 0;
+  }
+
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
 function Balances({
   assets,
   exclude,
@@ -24,6 +58,55 @@ function Balances({
   exclude: string[];
   onExcludeChange: (address: string, checked: boolean) => void;
 }): JSX.Element {
+  const [orderBy, setOrderBy] = useState<string | undefined>();
+  const [order, setOrder] = useState<TableSortDirection>(TableSortDirection.asc);
+  const rows = assets
+    .slice()
+    .sort(getComparator(order, orderBy))
+    .map((item: TokenBalance, index: number) => {
+      const token = item.tokenInfo || ethToken;
+
+      return {
+        id: `row${index}`,
+        cells: [
+          {
+            content: (
+              <Flex>
+                <Icon logoUri={token.logoUri} symbol={token.symbol} />
+                {token.name}
+              </Flex>
+            ),
+          },
+
+          { content: formatTokenValue(item.balance, token.decimals) },
+          { content: formatCurrencyValue(item.fiatBalance, CURRENCY) },
+
+          {
+            content: (
+              <Checkbox
+                label=""
+                name="exclude"
+                checked={exclude.includes(item.tokenInfo.address)}
+                onChange={(_, checked) => handleExclusion(item.tokenInfo.address, checked)}
+              />
+            ),
+          },
+        ],
+      };
+    });
+
+  const handleHeaderClick = (headerId: string) => {
+    if (headerId === 'exclude') {
+      return;
+    }
+
+    const newDirection =
+      orderBy === headerId && order === TableSortDirection.asc ? TableSortDirection.desc : TableSortDirection.asc;
+
+    setOrder(newDirection);
+    setOrderBy(headerId);
+  };
+
   const handleExclusion = (address: string, checked: boolean) => {
     onExcludeChange(address, checked);
   };
@@ -31,42 +114,15 @@ function Balances({
   return (
     <Table
       headers={[
-        { id: 'col1', label: 'Asset' },
-        { id: 'col2', label: 'Amount' },
-        { id: 'col3', label: `Value, ${CURRENCY}` },
-        { id: 'col4', label: 'Exclude' },
+        { id: 'tokenInfo.name', label: 'Asset' },
+        { id: 'balance', label: 'Amount' },
+        { id: 'fiatBalance', label: `Value, ${CURRENCY}` },
+        { id: 'exclude', label: 'Exclude' },
       ]}
-      rows={assets.map((item: TokenBalance, index: number) => {
-        const token = item.tokenInfo || ethToken;
-
-        return {
-          id: `row${index}`,
-          cells: [
-            {
-              content: (
-                <Flex>
-                  <Icon logoUri={token.logoUri} symbol={token.symbol} />
-                  {token.name}
-                </Flex>
-              ),
-            },
-
-            { content: formatTokenValue(item.balance, token.decimals) },
-            { content: formatCurrencyValue(item.fiatBalance, CURRENCY) },
-
-            {
-              content: (
-                <Checkbox
-                  label=""
-                  name="exclude"
-                  checked={exclude.includes(item.tokenInfo.address)}
-                  onChange={(_, checked) => handleExclusion(item.tokenInfo.address, checked)}
-                />
-              ),
-            },
-          ],
-        };
-      })}
+      rows={rows}
+      sortedByHeaderId={orderBy}
+      sortDirection={order}
+      onHeaderClick={handleHeaderClick}
     />
   );
 }
