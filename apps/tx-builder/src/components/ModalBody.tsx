8ef6758fa5ed9cf3f1b82fd92bdd7619b25299bd
Yago Pérez Vázquez <yagoperez@outlook.com>
2021-11-17 15:44:23 +0100

    Make the transaction detail break to next line for avoiding overflow

--- apps/tx-builder/src/components/ModalBody.tsx
+++ apps/tx-builder/src/components/ModalBody.tsx
@@ -1,9 +1,15 @@
-import { Button, Text } from '@gnosis.pm/safe-react-components';
 import React from 'react';
+import { Button, Text } from '@gnosis.pm/safe-react-components';
 import Box from '@material-ui/core/Box';
+import styled from 'styled-components';
 import { ProposedTransaction } from '../typings/models';
 
 type Props = { txs: Array<ProposedTransaction>; deleteTx: (index: number) => void };
+
+const WrappedText = styled(Text)`
+  word-break: break-all;
+  margin: 5px 0;
+`;
 
 export const ModalBody = ({ txs, deleteTx }: Props) => {
   return (
@@ -20,7 +26,7 @@ export const ModalBody = ({ txs, deleteTx }: Props) => {
           <Button size="md" variant="outlined" iconType="delete" color="error" onClick={() => deleteTx(index)}>
             {''}
           </Button>
-          <Text size="lg">{tx.description}</Text>
+          <WrappedText size="lg">{tx.description}</WrappedText>
         </Box>
       ))}
     </>
