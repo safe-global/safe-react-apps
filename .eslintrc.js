080e0b5cc4831e6c0ee72d14bd6ebff06ca6de83
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-12-02 14:24:03 +0100

    Bump dependencies with small changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'react-app',
+    'plugin:@typescript-eslint/recommended', // Plugin to use typescript with eslint
+    'prettier', // Add prettier rules to eslint
+    'plugin:prettier/recommended', // Plugin to use prettier rules with eslint
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
