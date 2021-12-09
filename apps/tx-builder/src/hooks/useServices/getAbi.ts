58f3d76e04fca2c51570fec8a7abf990b60dfca5
Yago Pérez Vázquez <yagoperez@outlook.com>
2021-12-09 18:09:13 +0100

    Change to gateway URL (Staging)

--- apps/tx-builder/src/hooks/useServices/getAbi.ts
+++ apps/tx-builder/src/hooks/useServices/getAbi.ts
@@ -19,7 +19,7 @@ const getProviderURL = (chain: string, address: string, urlProvider: PROVIDER): 
     case PROVIDER.SOURCIFY:
       return `https://sourcify.dev/server/files/${chain}/${address}`;
     case PROVIDER.TX_SERVICE:
-      return `https://safe-transaction.${chain}.gnosis.io/api/v1/contracts/${address}`;
+      return `https://safe-client.staging.gnosisdev.com/v1/chains/${chain}/contracts/${address}`;
     default:
       throw new Error('The Provider is not supported');
   }
@@ -50,7 +50,7 @@ const getAbi = async (address: string, chainInfo: ChainInfo): Promise<any> => {
   try {
     return await getAbiFromSourcify(address, chainInfo.chainId);
   } catch {
-    return await getAbiFromTxService(address, chainInfo.chainName?.toLowerCase());
+    return await getAbiFromTxService(address, chainInfo.chainId);
   }
 };
 
