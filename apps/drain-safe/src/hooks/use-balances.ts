b9420ce56f32b74fc45b91179221582979239efb
Yago Pérez Vázquez <yago.perez@gnosis.pm>
2021-12-21 11:38:06 +0100

    [Drain Safe] Update table component (#245)
    * Update current Table to new DataTable component in safe-react-components 0.9.6

--- apps/drain-safe/src/hooks/use-balances.ts
+++ apps/drain-safe/src/hooks/use-balances.ts
@@ -5,14 +5,14 @@ import { TokenBalance } from '@gnosis.pm/safe-apps-sdk';
 export type BalancesType = {
   error?: Error;
   assets: TokenBalance[];
-  excludedTokens: string[];
-  setExcludedTokens: (tokens: string[]) => void;
+  selectedTokens: string[];
+  setSelectedTokens: (tokens: string[]) => void;
 };
 
 function useBalances(safeAddress: string, chainId: number): BalancesType {
   const { sdk } = useSafeAppsSDK();
   const [assets, setAssets] = useState<TokenBalance[]>([]);
-  const [excludedTokens, setExcludedTokens] = useState<string[]>([]);
+  const [selectedTokens, setSelectedTokens] = useState<string[]>([]);
   const [error, setError] = useState<Error>();
 
   const loadBalances = useCallback(async () => {
@@ -24,6 +24,7 @@ function useBalances(safeAddress: string, chainId: number): BalancesType {
       const balances = await sdk.safe.experimental_getBalances({ currency: 'USD' });
 
       setAssets(balances.items);
+      setSelectedTokens(balances.items.map((token: any) => token.tokenInfo.address));
     } catch (err) {
       setError(err as Error);
     }
@@ -33,7 +34,7 @@ function useBalances(safeAddress: string, chainId: number): BalancesType {
     loadBalances();
   }, [loadBalances]);
 
-  return { error, assets, excludedTokens, setExcludedTokens };
+  return { error, assets, selectedTokens, setSelectedTokens };
 }
 
 export default useBalances;
