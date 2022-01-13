d4e976b8f883a3784a28adc354f9e43cecb0b760
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-01-13 15:55:35 +0100

    Initial commit

---
+++ apps/ramp-network/src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import styled from 'styled-components';
+import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
+import { Title, Loader } from '@gnosis.pm/safe-react-components';
+import { ChainInfo } from '@gnosis.pm/safe-apps-sdk';
+import RampWidget from './RampWidget';
+
+const Container = styled.div`
+  padding: 1rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ASSETS_BY_CHAIN: { [key: string]: string } = {
+  '1': 'ETH_*,ERC20_*',
+  '4': 'ETH_*,ERC20_*',
+  '56': 'BSC_*',
+  '100': 'XDAI_*',
+  '137': 'MATIC_*',
+  '43114': 'AVAX_*',
+};
+
+const SafeApp = (): React.ReactElement => {
+  const { sdk, safe } = useSafeAppsSDK();
+  const [chainInfo, setChainInfo] = useState<ChainInfo>();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const chainInfo = await sdk.safe.getChainInfo();
+        setChainInfo(chainInfo);
+      } catch (e) {
+        console.error('Unable to get chain info:', e);
+      }
+    })();
+  }, [sdk]);
+
+  const isChainSupported = useMemo(() => {
+    return chainInfo && Object.keys(ASSETS_BY_CHAIN).includes(chainInfo.chainId);
+  }, [chainInfo]);
+
+  if (!chainInfo) {
+    return <Loader size="lg" />;
+  }
+
+  return (
+    <Container>
+      {!isChainSupported && <Title size="lg">Network not supported</Title>}
+      {isChainSupported && (
+        <RampWidget
+          url={chainInfo.chainId === '4' ? 'https://ri-widget-staging.firebaseapp.com/' : ''}
+          address={safe.safeAddress}
+          assets={ASSETS_BY_CHAIN[chainInfo.chainId]}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default SafeApp;
---
+++ apps/ramp-network/src/RampWidget.tsx
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk';
+
+type Props = {
+  url?: string;
+  assets: string;
+  address: string;
+  onClose?: () => void;
+};
+
+const WIDGET_CLOSE_EVENT = 'WIDGET_CLOSE';
+
+const RampWidget = ({ url, assets, address, onClose }: Props) => {
+  const containerNode = useRef(null);
+  new RampInstantSDK({
+    url,
+    hostAppName: 'Ramp Network Safe App',
+    hostLogoUrl: 'https://docs.ramp.network/img/logo-1.svg',
+    swapAsset: assets,
+    userAddress: address,
+    containerNode: containerNode.current || undefined,
+  })
+    .on('*', (event) => {
+      if (event.type === WIDGET_CLOSE_EVENT) {
+        onClose?.();
+      }
+    })
+    .show();
+  return <div id="ramp-widget-container"></div>;
+};
+
+export default RampWidget;
