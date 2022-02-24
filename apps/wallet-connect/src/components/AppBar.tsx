8f2247a19162314ad692544b5d2958f0bc92d66a
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-02-24 13:47:18 +0100

    feat(wallet-connect): Check if app exists as a Safe App (#335)
    * Revamp UI
    
    * Add apps check to prompt the user to use the  Safe App instead

---
+++ apps/wallet-connect/src/components/AppBar.tsx
@@ -0,0 +1,35 @@
+import MuiAppBar from '@material-ui/core/AppBar';
+import styled from 'styled-components';
+import { Icon, Link, Text } from '@gnosis.pm/safe-react-components';
+
+const WALLET_CONNECT_HELP = 'https://help.gnosis-safe.io/en/articles/4356253-walletconnect-safe-app';
+
+const AppBar = () => {
+  return (
+    <StyledAppBar position="static" color="default">
+      <StyledAppBarText size="xl">Wallet Connect</StyledAppBarText>
+      <Link href={WALLET_CONNECT_HELP} target="_blank">
+        <Icon size="md" type="info" />
+      </Link>
+    </StyledAppBar>
+  );
+};
+
+const StyledAppBar = styled(MuiAppBar)`
+  && {
+    background: #fff;
+    height: 70px;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: row;
+    border-bottom: 2px solid #e8e7e6;
+  }
+`;
+
+const StyledAppBarText = styled(Text)`
+  font-size: 20px;
+  margin-left: 38px;
+  margin-right: 16px;
+`;
+
+export default AppBar;
