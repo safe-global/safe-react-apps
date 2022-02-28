eee3a028899c28e7ffc26b47c03290440745367d
Daniel Somoza <daniel.somoza@gnosis.pm>
2022-02-28 13:38:43 +0100

    Added Drag and drop JSX

--- apps/tx-builder/src/components/CreateNewBatchCard.tsx
+++ apps/tx-builder/src/components/CreateNewBatchCard.tsx
@@ -1,15 +1,53 @@
+import { ButtonLink, Icon, Text } from '@gnosis.pm/safe-react-components';
 import styled from 'styled-components';
 
 import { ReactComponent as CreateNewBatchSVG } from '../assets/add-new-batch.svg';
 
 const CreateNewBatchCard = () => {
-  return <StyledArrowToFormSVG />;
+  return (
+    <Wrapper>
+      <CreateNewBatchSVG />
+      <StyledDragAndDropFileContainer>
+        <Icon type="termsOfUse" size="sm" />
+        <StyledText size={'xl'}>Drag and drop a JSON file or</StyledText>
+        <StyledButtonLink color="primary" type="submit">
+          choose a file
+        </StyledButtonLink>
+      </StyledDragAndDropFileContainer>
+    </Wrapper>
+  );
 };
 
 export default CreateNewBatchCard;
 
-const StyledArrowToFormSVG = styled(CreateNewBatchSVG)`
-  position: absolute;
-  top: 64px;
-  left: 40px;
+const Wrapper = styled.div`
+  margin-top: 64px;
+`;
+
+const StyledDragAndDropFileContainer = styled.div`
+  box-sizing: border-box;
+  max-width: 420px;
+  border: 2px dashed #008c73;
+  border-radius: 8px;
+  background-color: #eaf7f4;
+  padding: 24px;
+  margin: 24px auto 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledText = styled(Text)`
+  margin-left: 4px;
+  color: #566976;
+`;
+
+const StyledButtonLink = styled(ButtonLink)`
+  padding: 0;
+  text-decoration: none;
+
+  && > p {
+    font-size: 16px;
+  }
 `;
