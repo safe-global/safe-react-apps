19ed01bce06c09bffd94ba80a100c5a9d389467a
Daniel Somoza <daniel.somoza@gnosis.pm>
2022-03-03 12:45:30 +0100

    removed console.log in getBatches fn

--- apps/tx-builder/src/lib/storage.tsx
+++ apps/tx-builder/src/lib/storage.tsx
@@ -43,7 +43,6 @@ const getBatches = async (chainInfo: ChainInfo) => {
           },
         })),
       };
-      console.log('parsedBatch');
       batches.push({ id: key, ...parsedBatch });
     });
     return batches;
