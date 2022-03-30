cda495699fae5ce4fd8c4ef372171229293c3211
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-03-30 12:51:22 +0200

    Bump deps

--- apps/tx-builder/config-overrides.js
+++ apps/tx-builder/config-overrides.js
@@ -1,26 +1,53 @@
-/* config-overrides.js */
- 
+const webpack = require('webpack');
+
 module.exports = {
-  // The function to use to create a webpack dev server configuration when running the development
-  // server with 'npm run start' or 'yarn start'.
-  // Example: set the dev server to use a specific certificate in https.
-  devServer: function(configFunction) {
-    // Return the replacement function for create-react-app to use to generate the Webpack
-    // Development Server config. "configFunction" is the function that would normally have
-    // been used to generate the Webpack Development server config - you can use it to create
-    // a starting configuration to then modify instead of having to create a config from scratch.
-    return function(proxy, allowedHost) {
-      // Create the default config by calling configFunction with the proxy/allowedHost parameters
+  webpack: function (config, env) {
+    const fallback = config.resolve.fallback || {};
+
+    // https://github.com/ChainSafe/web3.js#web3-and-create-react-app
+    Object.assign(fallback, {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+      url: require.resolve('url'),
+      // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
+      buffer: require.resolve('buffer'),
+    });
+
+    config.resolve.fallback = fallback;
+
+    config.plugins = (config.plugins || []).concat([
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+    ]);
+
+    // https://github.com/facebook/create-react-app/issues/11924
+    config.ignoreWarnings = [/to parse source map/i];
+
+    return config;
+  },
+  jest: function (config) {
+    return config;
+  },
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
       const config = configFunction(proxy, allowedHost);
 
       config.headers = {
-        "Access-Control-Allow-Origin": "\*",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-     };
- 
-      // Return your customised Webpack Development Server config.
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+      };
+
       return config;
     };
   },
-}
+  paths: function (paths) {
+    return paths;
+  },
+};
