7aa72dc47b69848f5c8e2dc3c3ea6c13f1f74cf8
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-03-31 11:54:00 +0200

    feat: bump dependencies (#409)
    * Bump deps
    
    * Bump eslint
    
    * Use eslint 8 action

--- apps/drain-safe/config-overrides.js
+++ apps/drain-safe/config-overrides.js
@@ -1,5 +1,34 @@
+const webpack = require('webpack');
+
 module.exports = {
-  webpack: function (config) {
+  webpack: function (config, env) {
+    const fallback = config.resolve.fallback || {};
+
+    // https://github.com/ChainSafe/web3.js#web3-and-create-react-app
+    Object.assign(fallback, {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+      url: require.resolve('url'),
+      // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
+      buffer: require.resolve('buffer'),
+    });
+
+    config.resolve.fallback = fallback;
+
+    config.plugins = (config.plugins || []).concat([
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+    ]);
+
+    // https://github.com/facebook/create-react-app/issues/11924
+    config.ignoreWarnings = [/to parse source map/i];
+
     return config;
   },
   jest: function (config) {
