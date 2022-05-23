e35971da2752b5e70bb1436dbfa224e09e2edc71
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-05-23 10:14:20 +0200

    Add some help for iframe commands

--- cypress/support/iframe.js
+++ cypress/support/iframe.js
@@ -1,5 +1,3 @@
-import '@testing-library/cypress/add-commands'
-
 const DEFAULT_OPTS = {
   log: true,
   timeout: 30000,
@@ -11,6 +9,19 @@ function sleep(timeout) {
   return new Promise(resolve => setTimeout(resolve, timeout))
 }
 
+// This command checks that an iframe has loaded onto the page
+// - This will verify that the iframe is loaded to any page other than 'about:blank'
+//   cy.frameLoaded()
+
+// - This will verify that the iframe is loaded to any url containing the given path part
+//   cy.frameLoaded({ url: 'https://google.com' })
+//   cy.frameLoaded({ url: '/join' })
+//   cy.frameLoaded({ url: '?some=query' })
+//   cy.frameLoaded({ url: '#/hash/path' })
+
+// - You can also give it a selector to check that a specific iframe has loaded
+//   cy.frameLoaded('#my-frame')
+//   cy.frameLoaded('#my-frame', { url: '/join' })
 Cypress.Commands.add('frameLoaded', (selector, opts) => {
   if (selector === undefined) {
     selector = DEFAULT_IFRAME_SELECTOR
@@ -68,6 +79,15 @@ Cypress.Commands.add('frameLoaded', (selector, opts) => {
   })
 })
 
+// This will cause subsequent commands to be executed inside of the given iframe
+// - This will verify that the iframe is loaded to any page other than 'about:blank'
+//   cy.iframe().find('.some-button').should('be.visible').click()
+//   cy.iframe().contains('Some hidden element').should('not.be.visible')
+//   cy.find('#outside-iframe').click() // this will be executed outside the iframe
+
+// - You can also give it a selector to find elements inside of a specific iframe
+//   cy.iframe('#my-frame').find('.some-button').should('be.visible').click()
+//   cy.iframe('#my-second-frame').contains('Some hidden element').should('not.be.visible')
 Cypress.Commands.add('iframe', (selector, opts) => {
   if (selector === undefined) {
     selector = DEFAULT_IFRAME_SELECTOR
@@ -94,6 +114,17 @@ Cypress.Commands.add('iframe', (selector, opts) => {
   })
 })
 
+// This can be used to execute a group of commands within an iframe
+// - This will verify that the iframe is loaded to any page other than 'about:blank'
+//   cy.enter().then(getBody => {
+//     getBody().find('.some-button').should('be.visible').click()
+//     getBody().contains('Some hidden element').should('not.be.visible')
+//   })
+// - You can also give it a selector to find elements inside of a specific iframe
+//   cy.enter('#my-iframe').then(getBody => {
+//     getBody().find('.some-button').should('be.visible').click()
+//     getBody().contains('Some hidden element').should('not.be.visible')
+//   })
 Cypress.Commands.add('enter', (selector, opts) => {
   if (selector === undefined) {
     selector = DEFAULT_IFRAME_SELECTOR
