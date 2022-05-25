b2f855c244176e8a1b63b67e5666da36e36bb971
Yago Pérez Vázquez <yagoperez@outlook.com>
2022-05-25 13:33:58 +0200

    Add some tests for transactions

---
+++ cypress/integration/tx-builder/transactions.spec.js
@@ -0,0 +1,42 @@
+describe('Testing transactions', () => {
+  it('should allow to send transactions', () => {
+    cy.connectWallet()
+
+    cy.visit(
+      `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
+        'TESTING_SAFE_ADDRESS',
+      )}/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder`,
+    )
+
+    const iframeSelector = `iframe[id="iframe-https://apps.gnosis-safe.io/tx-builder"]`
+
+    cy.findByText('Accept all').click({ force: true })
+    cy.findByText('Confirm').click({ force: true })
+    cy.frameLoaded(iframeSelector)
+
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/enter address or ens name/i)
+        .type('0x49d4450977E2c95362C13D3a31a09311E0Ea26A6')
+      getBody()
+        .findByLabelText(/paramAddress/i)
+        .type('0x49d4450977E2c95362C13D3a31a09311E0Ea26A6')
+      getBody()
+        .findByText(/add transaction/i)
+        .click()
+      getBody()
+        .findByText(/create batch/i)
+        .click()
+      getBody()
+        .findByText(/send batch/i)
+        .click()
+    })
+
+    cy.findByText(/transaction builder/i).should('be.visible')
+    cy.findByText(/submit/i).click()
+
+    cy.iframe(iframeSelector)
+      .findByText(/success!/i)
+      .should('be.visible')
+  })
+})
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,2 +1,29 @@
 import '@testing-library/cypress/add-commands'
 import './iframe'
+
+Cypress.Commands.add('connectWallet', () => {
+  cy.on('window:before:load', window => {
+    window.cypressConfig = {
+      connected: true,
+    }
+  })
+})
+
+Cypress.Commands.add('createSafe', () => {
+  cy.connectWallet()
+
+  cy.visit(`${Cypress.env('BASE_URL')}`)
+
+  cy.contains('a', 'Accept all').click()
+  cy.get('p').contains('Rinkeby').click()
+  cy.get('[data-testid=connected-wallet]').should('contain', 'e2e-wallet')
+  cy.contains('Create new Safe').click()
+  cy.contains('Continue').click()
+  cy.get('[data-testid=create-safe-name-field]').type('Test Safe')
+  cy.contains('button', 'Continue').click({ force: true })
+  cy.contains('button', 'Continue').click({ force: true })
+
+  cy.wait(500) // Not sure why without this ends with "Transaction underpriced"
+  cy.contains('button', 'Create').click()
+  cy.contains('Your Safe was created successfully', { timeout: 60000 })
+})
