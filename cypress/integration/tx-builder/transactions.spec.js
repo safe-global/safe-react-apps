aa37217127c0f0416a9a9ddee4ef637c002039a5
Jago Figueroa <jago@safe.global>
2022-06-08 12:54:26 +0200

    Added ENS and ABI input tests

--- cypress/integration/tx-builder/transactions.spec.js
+++ cypress/integration/tx-builder/transactions.spec.js
@@ -43,6 +43,84 @@ describe('Testing transactions', () => {
     //   .should('be.visible')
   })
 
+  it('should allow to send transactions to a ENS name', () => {
+    // cy.connectWallet()
+
+    cy.visit(
+      `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
+        'TESTING_SAFE_ADDRESS',
+      )}/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder`,
+    )
+
+    const iframeSelector = `iframe[id="iframe-https://apps.gnosis-safe.io/tx-builder"]`
+
+    cy.findByText('Accept all').click({ force: true })
+    cy.findByText('Confirm').click({ force: true })
+    cy.frameLoaded(iframeSelector)
+
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/enter address or ens name/i)
+        .type('jago-test.eth')
+      getBody()
+        .findByLabelText(/ETH value/i)
+        .type('0')
+      getBody()
+        .findByText(/add transaction/i)
+        .click()
+      getBody()
+        .findByText(/create batch/i)
+        .click()
+      getBody()
+        .findByText(/send batch/i)
+        .click()
+    })
+
+    cy.findByText(/transaction builder/i).should('be.visible')
+    cy.findAllByText('0xc6b82bA149CFA113f8f48d5E3b1F78e933e16DfD').should('be.visible')
+  })
+
+  it('should allow to send transactions from an ABI', () => {
+    // cy.connectWallet()
+
+    cy.visit(
+      `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
+        'TESTING_SAFE_ADDRESS',
+      )}/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder`,
+    )
+
+    const iframeSelector = `iframe[id="iframe-https://apps.gnosis-safe.io/tx-builder"]`
+
+    cy.findByText('Accept all').click({ force: true })
+    cy.findByText('Confirm').click({ force: true })
+    cy.frameLoaded(iframeSelector)
+
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/Enter ABI/i)
+        .type(
+          '[{{}"inputs":[{{}"internalType":"address","name":"_singleton","type":"address"{}}],"stateMutability":"nonpayable","type":"constructor"{}},{{}"stateMutability":"payable","type":"fallback"{}}]',
+        )
+      getBody()
+        .findByLabelText(/enter address or ens name/i)
+        .type('0x3bc83f41490BfD25bBB44eBCAc3761DFF4Ae50DA')
+      getBody()
+        .findByLabelText(/ETH value/i)
+        .type('0')
+      getBody()
+        .findByText(/add transaction/i)
+        .click()
+      getBody()
+        .findByText(/create batch/i)
+        .click()
+      getBody()
+        .findByText(/send batch/i)
+        .click()
+    })
+    cy.findByText(/transaction builder/i).should('be.visible')
+    cy.findAllByText('0x3bc83f41490BfD25bBB44eBCAc3761DFF4Ae50DA').should('be.visible')
+  })
+
   it('should not allow to create batch given invalid address', () => {
     cy.visit(
       `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
@@ -83,27 +161,17 @@ describe('Testing transactions', () => {
       getBody()
         .findAllByText('choose a file')
         .attachFile('test-batch.json', { subjectType: 'drag-n-drop' })
-      getBody()
-        .findAllByText('uploaded')
-        .wait(300)
-      getBody()
-        .find('button[title="Save to Library"]')
-        .click()
+      getBody().findAllByText('uploaded').wait(300)
+      getBody().find('button[title="Save to Library"]').click()
       getBody()
         .findByLabelText(/Batch name/i)
         .type('E2E test')
-      getBody()
-        .findAllByText('Create')
-        .should('not.be.disabled').click()        
+      getBody().findAllByText('Create').should('not.be.disabled').click()
       getBody()
         .findByText(/Your transaction library/i)
         .click()
-      getBody()
-        .find('button[title="Delete Batch"]')
-        .click()
-      getBody()
-        .findAllByText('Yes, delete').should('not.be.disabled')
-        .click()
+      getBody().find('button[title="Delete Batch"]').click()
+      getBody().findAllByText('Yes, delete').should('not.be.disabled').click()
       getBody()
         .findByText(/You don't have any saved batches./i)
         .should('be.visible')
