0b04e43ad1fb83d0b072c733c7742ee33e9e1096
Jago Figueroa <jago@safe.global>
2022-06-29 13:42:15 +0200

    added simulate, custom data tests & changed names

--- cypress/integration/tx-builder/transactions.spec.js
+++ cypress/integration/tx-builder/transactions.spec.js
@@ -1,5 +1,5 @@
 describe('Testing Tx-builder safe app', () => {
-  it('should allow to send transactions', () => {
+  it('should allow to send a batch', () => {
     cy.visit(
       `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
         'TESTING_SAFE_ADDRESS',
@@ -29,14 +29,13 @@ describe('Testing Tx-builder safe app', () => {
         .findByText(/send batch/i)
         .click()
     })
-
     cy.findByText(/transaction builder/i).should('be.visible')
     cy.findByText(/contract interaction/i).click()
     cy.findByText(/paramAddress/i).should('be.visible')
     cy.findAllByText('0x49d4450977E2c95362C13D3a31a09311E0Ea26A6').should('have.length', 2)
   })
 
-  it('should allow to send transactions to a ENS name', () => {
+  it('should allow to send a batch to a ENS name', () => {
     cy.visit(
       `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
         'TESTING_SAFE_ADDRESS',
@@ -66,12 +65,11 @@ describe('Testing Tx-builder safe app', () => {
         .findByText(/send batch/i)
         .click()
     })
-
     cy.findByText(/transaction builder/i).should('be.visible')
     cy.findAllByText('0xc6b82bA149CFA113f8f48d5E3b1F78e933e16DfD').should('be.visible')
   })
 
-  it('should allow to send transactions from an ABI', () => {
+  it('should allow to a batch from an ABI', () => {
     cy.visit(
       `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
         'TESTING_SAFE_ADDRESS',
@@ -110,7 +108,42 @@ describe('Testing Tx-builder safe app', () => {
     cy.findAllByText('0x3bc83f41490BfD25bBB44eBCAc3761DFF4Ae50DA').should('be.visible')
   })
 
-  it('should not allow to create batch given invalid address', () => {
+  it('should allow to send a batch using custom data', () => {
+    cy.visit(
+      `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
+        'TESTING_SAFE_ADDRESS',
+      )}/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder`,
+    )
+
+    const iframeSelector = `iframe[id="iframe-https://apps.gnosis-safe.io/tx-builder"]`
+
+    cy.findByText('Accept all').click({ force: true })
+    cy.findByText('Confirm').click({ force: true })
+    cy.frameLoaded(iframeSelector)
+
+    cy.enter(iframeSelector).then(getBody => {
+      getBody().find('input[type="checkbox"]').click()
+      getBody()
+        .findByLabelText(/enter address or ens name/i)
+        .type('0xc6b82bA149CFA113f8f48d5E3b1F78e933e16DfD')
+      getBody()
+        .findByLabelText(/ETH value/i)
+        .type('0')
+      getBody().findByLabelText(/Data/i).type('0x')
+      getBody()
+        .findByText(/add transaction/i)
+        .click()
+      getBody()
+        .findByText(/create batch/i)
+        .click()
+      getBody()
+        .findByText(/send batch/i)
+        .click()
+    })
+    cy.findByText(/transaction builder/i).should('be.visible')
+  })
+
+  it('should not allow to create a batch given invalid address', () => {
     cy.visit(
       `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
         'TESTING_SAFE_ADDRESS',
@@ -133,7 +166,7 @@ describe('Testing Tx-builder safe app', () => {
     })
   })
 
-  it('should allow to upload a batch, save it to my library, download it & then remove it', () => {
+  it('should allow to upload a batch, save it to the library, download it & remove it', () => {
     cy.visit(
       `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
         'TESTING_SAFE_ADDRESS',
@@ -170,5 +203,73 @@ describe('Testing Tx-builder safe app', () => {
         .should('be.visible')
     })
     cy.readFile('cypress/downloads/E2E test.json').should('exist')
+  })
+
+  it('should simulate a valid batch as successful', () => {
+    cy.visit(
+      `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
+        'TESTING_SAFE_ADDRESS',
+      )}/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder`,
+    )
+
+    const iframeSelector = `iframe[id="iframe-https://apps.gnosis-safe.io/tx-builder"]`
+
+    cy.findByText('Accept all').click({ force: true })
+    cy.findByText('Confirm').click({ force: true })
+    cy.frameLoaded(iframeSelector)
+
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/enter address or ens name/i)
+        .type('0xc6b82bA149CFA113f8f48d5E3b1F78e933e16DfD')
+      getBody()
+        .findByLabelText(/ETH value/i)
+        .type('0')
+      getBody()
+        .findByText(/add transaction/i)
+        .click()
+      getBody()
+        .findByText(/create batch/i)
+        .click()
+      getBody()
+        .findByText(/Simulate/i)
+        .click()
+      getBody().findByText('Transfer').should('be.visible')
+      getBody().findByText('Success').should('be.visible')
+    })
+  })
+
+  it('should simulate an invalid batch as failed', () => {
+    cy.visit(
+      `${Cypress.env('BASE_URL')}/${Cypress.env('NETWORK_PREFIX')}:${Cypress.env(
+        'TESTING_SAFE_ADDRESS',
+      )}/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder`,
+    )
+
+    const iframeSelector = `iframe[id="iframe-https://apps.gnosis-safe.io/tx-builder"]`
+
+    cy.findByText('Accept all').click({ force: true })
+    cy.findByText('Confirm').click({ force: true })
+    cy.frameLoaded(iframeSelector)
+
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/enter address or ens name/i)
+        .type('rin:0x91dB860c37E83dab0A4A005E209577E64c61EEfA')
+      getBody()
+        .findByLabelText(/ETH value/i)
+        .type('100')
+      getBody()
+        .findByText(/add transaction/i)
+        .click()
+      getBody()
+        .findByText(/create batch/i)
+        .click()
+      getBody()
+        .findByText(/Simulate/i)
+        .click()
+      getBody().findByText('fallback').should('be.visible')
+      getBody().findByText('Failed').should('be.visible')
+    })
   })
 })
