4925841470769f1d5c6b8dd1bbe110fe94f1e402
Usame Algan <usame@safe.global>
2022-09-08 16:38:28 +0200

    fix: fix(safe-claiming-app): Update vesting data url, add governance forum post url"

--- apps/safe-claiming-app/src/config/constants.ts
+++ apps/safe-claiming-app/src/config/constants.ts
@@ -15,25 +15,28 @@ export const SAFE_TOKEN_ADDRESS = "0xCFf1b0FdE85C102552D1D96084AF148f478F964A"
 
 // FIXME testing airdrop address
 export const USER_AIRDROP_ADDRESS = "0x6C6ea0B60873255bb670F838b03db9d9a8f045c4"
+
 //FIXME testing airdrop address
 export const ECOSYSTEM_AIRDROP_ADDRESS =
   "0x82F1267759e9Bea202a46f8FC04704b6A5E2Af77"
 
+// TODO: Update URL once deployed to prod
 export const GUARDIANS_URL =
   "https://5afe.github.io/claiming-app-data/resources/data/guardians.json"
 
+// TODO: Update URL once deployed to prod
 export const GUARDIANS_IMAGE_URL =
   "https://5afe.github.io/claiming-app-data/resources/data/images/"
 
 export const VESTING_URL =
-  "https://5afe.github.io/claiming-app-data/resources/data/allocations/4/"
+  "https://safe-claiming-app-data.gnosis-safe.io/allocations/"
 
 export const FORUM_URL = "https://forum.gnosis-safe.io"
 
 export const DISCORD_URL = "https://discord.gg/gXK3gt8w3D"
 
-// TODO: Link to an upcoming forum post
-export const GOVERNANCE_URL = "https://forum.gnosis-safe.io"
+export const GOVERNANCE_URL =
+  "https://forum.gnosis-safe.io/t/how-to-safedao-governance-process/846"
 
 export const FULL_PROPOSAL_URL =
   "https://forum.gnosis-safe.io/t/safe-voting-power-and-circulating-supply/558"
