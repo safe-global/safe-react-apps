8df7b7f89015dd66edc6e92cb5c0c3a46cb729fa
Daniel <25051234+dasanra@users.noreply.github.com>
2022-10-11 18:32:16 +0200

    refactor(drain-safe): use Safe Apps provider on drain-safe

--- apps/drain-safe/src/hooks/useWeb3.ts
+++ apps/drain-safe/src/hooks/useWeb3.ts
@@ -1,34 +1,19 @@
 import { useEffect, useState } from 'react'
 import Web3 from 'web3'
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
+import { SafeAppProvider } from '@gnosis.pm/safe-apps-provider'
 
 function useWeb3() {
   const [web3, setWeb3] = useState<Web3 | undefined>()
-  const { sdk } = useSafeAppsSDK()
+  const { safe, sdk } = useSafeAppsSDK()
 
   useEffect(() => {
-    const setWeb3Instance = async () => {
-      const chainInfo = await sdk.safe.getChainInfo()
+    const safeProvider = new SafeAppProvider(safe, sdk)
+    // @ts-expect-error Web3 is complaining about some missing properties from websocket provider
+    const web3Instance = new Web3(safeProvider)
 
-      if (!chainInfo) {
-        return
-      }
-
-      const rpcUrlGetter = rpcUrlGetterByNetwork[chainInfo.chainId as CHAINS]
-
-      if (!rpcUrlGetter) {
-        throw Error(`RPC URL not defined for ${chainInfo.chainName} chain`)
-      }
-
-      const rpcUrl = rpcUrlGetter(process.env.REACT_APP_RPC_TOKEN)
-
-      const web3Instance = new Web3(rpcUrl)
-
-      setWeb3(web3Instance)
-    }
-
-    setWeb3Instance()
-  }, [sdk.safe])
+    setWeb3(web3Instance)
+  }, [safe, sdk])
 
   return {
     web3,
@@ -36,41 +21,3 @@ function useWeb3() {
 }
 
 export default useWeb3
-
-export enum CHAINS {
-  MAINNET = '1',
-  MORDEN = '2',
-  ROPSTEN = '3',
-  RINKEBY = '4',
-  GOERLI = '5',
-  OPTIMISM = '10',
-  KOVAN = '42',
-  BSC = '56',
-  XDAI = '100',
-  POLYGON = '137',
-  ENERGY_WEB_CHAIN = '246',
-  ARBITRUM = '42161',
-  AVALANCHE = '43114',
-  VOLTA = '73799',
-  AURORA = '1313161554',
-}
-
-export const rpcUrlGetterByNetwork: {
-  [key in CHAINS]: null | ((token?: string) => string)
-} = {
-  [CHAINS.MAINNET]: token => `https://mainnet.infura.io/v3/${token}`,
-  [CHAINS.MORDEN]: null,
-  [CHAINS.ROPSTEN]: null,
-  [CHAINS.RINKEBY]: token => `https://rinkeby.infura.io/v3/${token}`,
-  [CHAINS.GOERLI]: token => `https://goerli.infura.io/v3/${token}`,
-  [CHAINS.OPTIMISM]: () => 'https://mainnet.optimism.io',
-  [CHAINS.KOVAN]: null,
-  [CHAINS.BSC]: () => 'https://bsc-dataseed.binance.org',
-  [CHAINS.XDAI]: () => 'https://rpc.gnosischain.com',
-  [CHAINS.POLYGON]: () => 'https://rpc-mainnet.matic.network',
-  [CHAINS.ENERGY_WEB_CHAIN]: () => 'https://rpc.energyweb.org',
-  [CHAINS.ARBITRUM]: () => 'https://arb1.arbitrum.io/rpc',
-  [CHAINS.AVALANCHE]: () => 'https://api.avax.network/ext/bc/C/rpc',
-  [CHAINS.VOLTA]: () => 'https://volta-rpc.energyweb.org',
-  [CHAINS.AURORA]: () => 'https://mainnet.aurora.dev',
-}
