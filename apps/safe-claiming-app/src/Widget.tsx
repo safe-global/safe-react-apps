a02a41677214a4ddb5b771eb5c243a3c326c25f3
Diogo Soares <diogo.lsoares@gmail.com>
2022-11-14 16:36:19 +0100

    add fetch delegate

--- apps/safe-claiming-app/src/Widget.tsx
+++ apps/safe-claiming-app/src/Widget.tsx
@@ -1,19 +1,39 @@
 import { Box, Typography } from "@mui/material"
+import { useMemo } from "react"
+import { AppState } from "src/App"
 import { ReactComponent as SafeIcon } from "src/assets/images/safe-token.svg"
 import { SelectedDelegate } from "src/components/steps/Claim/SelectedDelegate"
+import { useDelegate } from "src/hooks/useDelegate"
+import { useDelegatesFile } from "src/hooks/useDelegatesFile"
+import { sameAddress } from "src/utils/addresses"
 
-const Widget = () => {
-  const delegate = { address: "0x123" }
+type Props = {
+  state?: AppState
+}
+
+const Widget = ({ state }: Props) => {
+  const [delegates] = useDelegatesFile()
+  const delegateAddressFromContract = useDelegate()
+
+  const currentDelegate = useMemo(() => {
+    if (delegateAddressFromContract) {
+      const registeredDelegateFromData = delegates.find((entry) =>
+        sameAddress(entry.address, delegateAddressFromContract)
+      )
+      return (
+        registeredDelegateFromData || { address: delegateAddressFromContract }
+      )
+    }
+  }, [delegateAddressFromContract, delegates])
+
   return (
     <Box
       display="flex"
       flexDirection="column"
       alignItems="center"
-      p={1}
-      sx={{
-        height: "300px",
-        background: ({ palette }) => palette.background.paper,
-      }}
+      height="300px"
+      p={3}
+      sx={{ backgroundColor: "background.paper" }}
     >
       <Typography variant="subtitle1" fontWeight={700} color="primary.main">
         Voting power{" "}
@@ -31,12 +51,12 @@ const Widget = () => {
         {/* TODO: fetch claimed value */}
         You've already claimed 342.32 SAFE
       </Typography>
-      {delegate && (
+      {currentDelegate && (
         <Box width={1}>
           <Typography variant="caption" marginBottom={1}>
             Delegated to
           </Typography>
-          <SelectedDelegate delegate={delegate} onClick={() => {}} />
+          <SelectedDelegate delegate={currentDelegate} onClick={() => {}} />
         </Box>
       )}
     </Box>
