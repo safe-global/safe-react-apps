da08afe75906ec71e658e509e113f366284e7e50
Diogo Soares <diogo.lsoares@gmail.com>
2022-11-14 17:11:40 +0100

    calculate voting power and claimed tokens

--- apps/safe-claiming-app/src/Widget.tsx
+++ apps/safe-claiming-app/src/Widget.tsx
@@ -1,19 +1,28 @@
 import { Box, Typography } from "@mui/material"
+import { BigNumber, ethers } from "ethers"
 import { useMemo } from "react"
 import { AppState } from "src/App"
 import { ReactComponent as SafeIcon } from "src/assets/images/safe-token.svg"
 import { SelectedDelegate } from "src/components/steps/Claim/SelectedDelegate"
+import { useAirdropFile } from "src/hooks/useAirdropFile"
 import { useDelegate } from "src/hooks/useDelegate"
 import { useDelegatesFile } from "src/hooks/useDelegatesFile"
+import { useFetchVestingStatus } from "src/hooks/useFetchVestingStatus"
+import { useTokenBalance } from "src/hooks/useTokenBalance"
 import { sameAddress } from "src/utils/addresses"
+import { formatAmount } from "src/utils/format"
 
-type Props = {
-  state?: AppState
-}
-
-const Widget = ({ state }: Props) => {
+const Widget = () => {
   const [delegates] = useDelegatesFile()
   const delegateAddressFromContract = useDelegate()
+  const balance = useTokenBalance()
+
+  const [vestings, isVestingLoading, vestingFileError] = useAirdropFile()
+  const [userVesting, ecosystemVesting, investorVesting] = vestings
+
+  const totalAllocation = BigNumber.from(userVesting?.amount || 0)
+    .add(ecosystemVesting?.amount || 0)
+    .add(investorVesting?.amount || 0)
 
   const currentDelegate = useMemo(() => {
     if (delegateAddressFromContract) {
@@ -25,6 +34,27 @@ const Widget = ({ state }: Props) => {
       )
     }
   }, [delegateAddressFromContract, delegates])
+
+  // fetch the claimed amount
+  const [userVestingStatus, userVestingStatusError] = useFetchVestingStatus(
+    userVesting?.vestingId,
+    userVesting?.contract
+  )
+
+  const [ecosystemVestingStatus, ecosystemVestingStatusError] =
+    useFetchVestingStatus(
+      ecosystemVesting?.vestingId,
+      ecosystemVesting?.contract
+    )
+
+  const [investorVestingStatus, investorVestingStatusError] =
+    useFetchVestingStatus(investorVesting?.vestingId, investorVesting?.contract)
+
+  const totalClaimed = BigNumber.from(userVestingStatus?.amountClaimed || 0)
+    .add(ecosystemVestingStatus?.amountClaimed || 0)
+    .add(investorVestingStatus?.amountClaimed || 0)
+
+  const votingPower = totalAllocation.add(balance).sub(totalClaimed)
 
   return (
     <Box
@@ -41,15 +71,13 @@ const Widget = ({ state }: Props) => {
       <Box display="flex" alignItems="center" gap={1} mb={1}>
         <SafeIcon />
         <Typography variant="h5">
-          {/* TODO: fetch total allocation */}
-          {/* {formatAmount(Number(ethers.utils.formatEther(totalAllocation)), 2)}{" "} */}
-          528.69
+          {formatAmount(Number(ethers.utils.formatEther(votingPower)), 2)}{" "}
         </Typography>
       </Box>
 
       <Typography variant="subtitle2" mb="22px">
-        {/* TODO: fetch claimed value */}
-        You've already claimed 342.32 SAFE
+        You've already claimed{" "}
+        {formatAmount(Number(ethers.utils.formatEther(totalClaimed)), 2)} SAFE
       </Typography>
       {currentDelegate && (
         <Box width={1}>
---
+++ apps/safe-claiming-app/src/hooks/useTokenBalance.ts
@@ -0,0 +1,41 @@
+import { useSafeAppsSDK } from "@gnosis.pm/safe-apps-react-sdk"
+import { BigNumber } from "ethers"
+import { useEffect, useState } from "react"
+import { CHAIN_CONSTANTS } from "src/config/constants"
+import { SafeToken__factory } from "src/types/contracts/factories/SafeToken__factory"
+import { getWeb3Provider } from "src/utils/getWeb3Provider"
+
+/**
+ * Fetches the current token balance.
+ */
+export const useTokenBalance = () => {
+  const [balance, setBalance] = useState(BigNumber.from(0))
+  const { safe, sdk } = useSafeAppsSDK()
+  const web3Provider = getWeb3Provider(safe, sdk)
+  const chainConstants = CHAIN_CONSTANTS[safe.chainId]
+
+  useEffect(() => {
+    let isMounted = true
+
+    const fetchTokenBalance = async () => {
+      try {
+        const newBalance = await SafeToken__factory.connect(
+          chainConstants.SAFE_TOKEN_ADDRESS,
+          web3Provider
+        ).balanceOf(safe.safeAddress)
+
+        isMounted && setBalance(newBalance)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    if (chainConstants) {
+      fetchTokenBalance()
+    }
+    return () => {
+      isMounted = false
+    }
+  }, [chainConstants, safe.safeAddress, web3Provider])
+
+  return balance
+}
