e40e92e6e878650e7a290070f5246ab9a273b01c
Yago Pérez Vázquez <yago@safe.global>
2022-11-15 12:55:16 +0100

    Pluralize category constant

--- apps/tx-builder/src/lib/analytics.ts
+++ apps/tx-builder/src/lib/analytics.ts
@@ -1,9 +1,9 @@
-const SAFE_APPS_ANALYTIC_CATEGORY = 'safe-apps-analytics'
+const SAFE_APPS_ANALYTICS_CATEGORY = 'safe-apps-analytics'
 
 export const trackSafeAppEvent = (action: string, label?: string) => {
   window.parent.postMessage(
     {
-      category: SAFE_APPS_ANALYTIC_CATEGORY,
+      category: SAFE_APPS_ANALYTICS_CATEGORY,
       action,
       label,
       safeAppName: 'Transaction Builder',
