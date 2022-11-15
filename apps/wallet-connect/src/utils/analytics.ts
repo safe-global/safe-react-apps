78a3ff0cc42004726f6bb0025bf66becf7f98fed
Yago Pérez Vázquez <yago@safe.global>
2022-11-15 12:54:36 +0100

    Pluralize category constant

--- apps/wallet-connect/src/utils/analytics.ts
+++ apps/wallet-connect/src/utils/analytics.ts
@@ -1,9 +1,9 @@
-const SAFE_APPS_ANALYTIC_CATEGORY = 'safe-apps-analytics'
+const SAFE_APPS_ANALYTICS_CATEGORY = 'safe-apps-analytics'
 
 export const trackSafeAppEvent = (action: string, connectedPeer?: string) => {
   window.parent.postMessage(
     {
-      category: SAFE_APPS_ANALYTIC_CATEGORY,
+      category: SAFE_APPS_ANALYTICS_CATEGORY,
       action,
       label: connectedPeer,
       safeAppName: 'WalletConnect',
