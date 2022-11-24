ba6b313e19fe940a89e11f7ecffcd6a2c5957f42
Yago Pérez Vázquez <yago@safe.global>
2022-11-24 16:10:09 +0100

    Add ethereum account in header

---
+++ apps/mmi/src/lib/utils.ts
@@ -0,0 +1,7 @@
+const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/
+
+export const truncateEthAddress = (address: string) => {
+  const match = address.match(truncateRegex)
+  if (!match) return address
+  return `${match[1]}…${match[2]}`
+}
