bb454c82d30c33ac28582f6b0199924abea7703c
Yago Pérez Vázquez <yago@safe.global>
2023-01-30 13:57:37 +0100

    Update to beta.1

--- apps/mmi/src/index.tsx
+++ apps/mmi/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SafeProvider from '@gnosis.pm/safe-apps-react-sdk'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { createSafeTheme } from '@safe-global/safe-react-components'
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
+import { SafeThemeProvider } from '@safe-global/safe-react-components'
 
 import App from './App'
 
@@ -10,12 +10,16 @@ import '@safe-global/safe-react-components/dist/fonts.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={createSafeTheme('dark')}>
-      <CssBaseline />
-      <SafeProvider loader={<h1>Waiting for Safe...</h1>}>
-        <App />
-      </SafeProvider>
-    </ThemeProvider>
+    <SafeThemeProvider mode="dark">
+      {(safeTheme: Theme) => (
+        <ThemeProvider theme={safeTheme}>
+          <CssBaseline />
+          <SafeProvider loader={<h1>Waiting for Safe...</h1>}>
+            <App />
+          </SafeProvider>
+        </ThemeProvider>
+      )}
+    </SafeThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
