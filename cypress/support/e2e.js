a0823d7b3c16be73bdbcb0c6635b8b2b6491d3f2
Yago Pérez Vázquez <yago@safe.global>
2023-03-13 12:53:39 +0100

    fix(e2e): Add some fixes in e2e tests (#681)

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -10,12 +10,29 @@ const chains = [1, 5, 10, 56, 100, 137, 42161, 43114, 73799, 1313161554]
 let warningCheckedCustomApps = []
 const drainSafeUrl = Cypress.env('DRAIN_SAFE_URL')
 
-if (drainSafeUrl) {
+// TODO: Remove this once all the safe apps are deployed on the same domain in each environment
+if (drainSafeUrl && drainSafeUrl.includes('safereactapps.review-react-hr.5afe.dev')) {
   warningCheckedCustomApps.push(new URL(drainSafeUrl).origin)
+} else {
+  warningCheckedCustomApps = [
+    'https://apps.gnosis-safe.io',
+    'https://safe-apps.dev.5afe.dev',
+    'https://apps-portal.safe.global',
+  ]
 }
 
 Cypress.Commands.add('visitSafeApp', (visitUrl, appUrl) => {
+  if (appUrl) {
+    cy.intercept('GET', `${appUrl}/manifest.json`, {
+      name: 'App',
+      description: 'The App',
+      iconPath: 'logo.svg',
+      safe_apps_permissions: [],
+    })
+  }
+
   cy.on('window:before:load', async window => {
+    // Avoid to show the disclaimer and unknown apps warning
     window.localStorage.setItem(
       INFO_MODAL_KEY,
       JSON.stringify({
@@ -31,26 +48,6 @@ Cypress.Commands.add('visitSafeApp', (visitUrl, appUrl) => {
         ),
       }),
     )
-
-    try {
-      const response = await fetch(`${appUrl}/manifest.json`)
-      const manifest = await response.json()
-      const { safe_apps_permissions } = manifest
-
-      if (safe_apps_permissions) {
-        window.localStorage.setItem(
-          BROWSER_PERMISSIONS_KEY,
-          JSON.stringify({
-            [new URL(appUrl).origin]: safe_apps_permissions.map(permission => ({
-              feature: permission,
-              status: 'granted',
-            })),
-          }),
-        )
-      }
-    } catch {
-      console.error(`Error retrieving manifest for ${appUrl}`)
-    }
 
     window.localStorage.setItem('SAFE_v2__lastWallet', JSON.stringify('E2E Wallet'))
   })
