523cb6e38341b455639a9bc9d55091bbdc0d09db
iamacook <aaron@safe.global>
2023-03-14 12:04:11 +0100

    chore(safe-claiming-app): remove Safe Claiming App

--- apps/safe-claiming-app/contracts/deps.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.7.0 <0.9.0;
-
-// These two contract import all others in `safe-token`
-import { Airdrop } from "safe-token/contracts/Airdrop.sol";
-import { SafeToken } from "safe-token/contracts/SafeToken.sol";
--- apps/safe-claiming-app/src/hooks/useSnapshotSpace.ts
+++
@@ -1,7 +0,0 @@
-import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk"
-import { CHAIN_CONSTANTS } from "src/config/constants"
-
-export const useSafeSnapshotSpace = () => {
-  const { safe } = useSafeAppsSDK()
-  return CHAIN_CONSTANTS[safe.chainId]?.DELEGATE_ID
-}
