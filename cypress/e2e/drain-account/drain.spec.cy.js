cdd38b82676ba7c1af6971a2877055184521b86c
Jago Figueroa <92332710+JagoFigueroa@users.noreply.github.com>
2023-03-21 17:42:59 +0100

    test: adding more tests for drain spec (#684)
    * adding more tests for drain spec
    
    * temporary change to run tests on dev
    
    * Reverting prev commit after checking tests are ok

--- cypress/e2e/drain-account/drain.spec.cy.js
+++ cypress/e2e/drain-account/drain.spec.cy.js
@@ -30,25 +30,6 @@ describe('Testing Drain Account safe app', { defaultCommandTimeout: 12000 }, () 
     cy.findByRole('button', { name: 'Action 3 transfer' })
   })
 
-  it('should not allow to perform a drain when no recipient is selected', () => {
-    cy.enter(iframeSelector).then(getBody => {
-      getBody().findAllByText('Transfer everything').click()
-      getBody().findByText(/please enter a valid recipient address/i)
-    })
-  })
-
-  it('should not allow to perform a drain when no assets are selected', () => {
-    cy.enter(iframeSelector).then(getBody => {
-      getBody()
-        .findByLabelText(/select all rows checkbox/i)
-        .click()
-      getBody()
-        .findByLabelText(/recipient/i)
-        .type('0x49d4450977E2c95362C13D3a31a09311E0Ea26A6')
-      getBody().findAllByText('No tokens selected').should('be.visible')
-    })
-  })
-
   it('should allow to perform a partial drain', () => {
     cy.enter(iframeSelector).then(getBody => {
       getBody()
@@ -70,5 +51,70 @@ describe('Testing Drain Account safe app', { defaultCommandTimeout: 12000 }, () 
     })
     cy.findByRole('button', { name: 'Action 1' })
     cy.findByRole('button', { name: 'Action 2 transfer' })
+  })
+
+  it('should allow to perform a drain when a ENS is specified', () => {
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/recipient/i)
+        .type('goerli-test-safe.eth')
+        .wait(2000)
+      getBody().findAllByText('Transfer everything').click()
+    })
+    cy.findByRole('button', { name: 'Action 1 transfer' })
+    cy.findByRole('button', { name: 'Action 2' })
+    cy.findByRole('button', { name: 'Action 3 transfer' })
+  })
+
+  it('should keep previous data when drain is cancelled', () => {
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/recipient/i)
+        .type('0x49d4450977E2c95362C13D3a31a09311E0Ea26A6')
+      getBody().findAllByText('Transfer everything').click()
+    })
+    cy.findByRole('button', { name: 'Cancel' }).click()
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findAllByText('Transfer everything').should('be.visible')
+    })
+  })
+
+  it('should not allow to perform a drain when no recipient is selected', () => {
+    cy.enter(iframeSelector).then(getBody => {
+      getBody().findAllByText('Transfer everything').click()
+      getBody().findByText(/please enter a valid recipient address/i)
+    })
+  })
+
+  it('should not allow to perform a drain when an invalid recipient is selected', () => {
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/recipient/i)
+        .type('0x49d4450977E2c95362C13D3a31a09311E0Ea26A')
+      getBody().findAllByText('Transfer everything').click()
+      getBody().findByText(/please enter a valid recipient address/i)
+    })
+  })
+
+  it('should not allow to perform a drain when no assets are selected', () => {
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/select all rows checkbox/i)
+        .click()
+      getBody()
+        .findByLabelText(/recipient/i)
+        .type('0x49d4450977E2c95362C13D3a31a09311E0Ea26A6')
+      getBody().findAllByText('No tokens selected').should('be.visible')
+    })
+  })
+
+  it('should not allow to perform a drain when no assets and recipient are selected', () => {
+    cy.enter(iframeSelector).then(getBody => {
+      getBody()
+        .findByLabelText(/select all rows checkbox/i)
+        .click()
+      getBody().findAllByText('No tokens selected').should('be.visible')
+    })
   })
 })
