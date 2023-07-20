1798a04a9a1320fc9cabb023bfb23f80f715b8a5
Daniel <25051234+dasanra@users.noreply.github.com>
2023-07-20 17:48:57 +0200

    chore: bump safe apps dependencies

--- apps/drain-safe/src/setupTests.ts
+++ apps/drain-safe/src/setupTests.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom/extend-expect'
+
+// Jest is not able to use this function from node, which is used at viem v1.3.0
+// We need to import it manually
+import { TextEncoder } from 'util'
+
+global.TextEncoder = TextEncoder
+// END
