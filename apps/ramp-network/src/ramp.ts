3e63b26ca9eb9844cfc0de942ef88d5c132202da
katspaugh <381895+katspaugh@users.noreply.github.com>
2024-08-20 07:28:12 +0200

    feat(ramp): add Optimism and Linea (#814)

--- apps/ramp-network/src/ramp.ts
+++ apps/ramp-network/src/ramp.ts
@@ -1,14 +1,12 @@
-import { ChainInfo } from '@safe-global/safe-apps-sdk'
 import { RampInstantEvent, RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
 import { RAMP_API_KEY } from './constants'
 
-const RINKEBY_STAGING_URL = 'https://ri-widget-staging.firebaseapp.com/'
 const WIDGET_CLOSE_EVENT = 'WIDGET_CLOSE'
 const PURCHASE_CREATED_EVENT = 'PURCHASE_CREATED'
 
 export const ASSETS_BY_CHAIN: { [key: string]: string } = {
-  '1': 'ETH_*,ERC20_*',
-  '4': 'ETH_*,ERC20_*',
+  '1': 'ETH_*',
+  '10': 'OPTIMISM_*',
   '56': 'BSC_*',
   '137': 'MATIC_*',
   '100': 'XDAI_*',
@@ -18,10 +16,7 @@ export const ASSETS_BY_CHAIN: { [key: string]: string } = {
   '1101': 'POLYGONZKEVM_*',
   '42161': 'ARBITRUM_*',
   '42220': 'CELO_*',
-}
-
-export const getRampWidgetUrl = (chainInfo: ChainInfo) => {
-  return chainInfo?.chainId === '4' ? RINKEBY_STAGING_URL : ''
+  '59144': 'LINEA_*',
 }
 
 type RampWidgetInitializer = {
